f341bec4ba2b41bbc46c4c537af3577020de310f
David Moina <davemolina777@gmail.com>
2021-01-25 17:14:05 -0600

    commit tree 37b09017ce7d66aba76bc5d7a8a6304f929276f6

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, "p1n3-app13"
+    end
+
+    get '/' do
+        erb :index
+    end
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        #binding.pry
+        erb :checkout
+    end
 end
